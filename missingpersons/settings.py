95b93cca75b20ca578baec778dfa20e2046d588e
parkerwatts21 <parkerwatts21@gmail.com>
2023-04-07 14:08:04 -0600

    Added the database to online

--- missingpersons/settings.py
+++ missingpersons/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'missingpersons.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'missingpersons',
+        'USER': 'postgres',
+        'PASSWORD': 'pw1007',
+        'HOST': 'localhost',
     }
-}
-
+}  
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- personpages/models.py
+++ personpages/models.py
@@ -1,4 +1,18 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class MissingPerson (models.Model) :
+    date_missing = models.DateField(default = datetime.today)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    age_at_missing = models.IntegerField(default=0)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=5)
+    gender = models.CharField(max_length=5)
+    race = models.CharField(max_length=5)
+def full_name(self):
+    return '%s %s' % (self.first_name, self.last_name)
+def __str__(self):
+    return (self.full_name)  
 
