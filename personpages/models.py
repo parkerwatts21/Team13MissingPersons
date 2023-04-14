f90805a8888bef639d9688857b21372f0237654d
parkerwatts21 <parkerwatts21@gmail.com>
2023-04-13 18:58:10 -0600

    Update models.py

--- personpages/models.py
+++ personpages/models.py
@@ -15,7 +15,5 @@ class MissingPerson (models.Model) :
     race = models.CharField(max_length=5)
     class Meta:
         db_table = 'MissingTable'
-def full_name(self):
-    return '%s %s' % (self.first_name, self.last_name)
-def __str__(self):
-    return (self.first_name + " " + self.last_name)  
+    def __str__(self):
+        return (self.first_name + " " + self.last_name)  
